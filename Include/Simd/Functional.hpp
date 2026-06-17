1e62baecbda80b77e28996985010ce323120a6b8
Paweł Mrocheń <pmrochen@users.noreply.github.com>
2026-06-17 17:51:15 +0200

    Add int4 support

--- Include/Simd/Functional.hpp
+++ Include/Simd/Functional.hpp
@@ -9,25 +9,37 @@
 #include <cstddef>
 #include "Intrinsics.hpp"
 
-#if SIMD_HAS_FLOAT4
-
 namespace std {
 
 template<typename T>
 struct equal_to;
 
+#if SIMD_HAS_FLOAT4
 template<>
 struct equal_to<::simd::float4>
 {
 	bool operator()(const ::simd::float4& v1, const ::simd::float4& v2) const noexcept
 	{
-		return ::simd::all4(::simd::equal(v1, v2));
+		return ::simd::all(::simd::equal(v1, v2));
 	}
 };
+#endif /* SIMD_HAS_FLOAT4 */
+
+#if SIMD_HAS_INT4
+template<>
+struct equal_to<::simd::int4>
+{
+	bool operator()(const ::simd::int4& v1, const ::simd::int4& v2) const noexcept
+	{
+		return ::simd::all(::simd::equal(v1, v2));
+	}
+};
+#endif /* SIMD_HAS_INT4 */
 
 template<typename T>
 struct hash;
 
+#if SIMD_HAS_FLOAT4
 template<>
 struct hash<::simd::float4>
 {
@@ -36,7 +48,17 @@ struct hash<::simd::float4>
         return ::simd::hash(v);
     }
 };
+#endif /* SIMD_HAS_FLOAT4 */
+
+#if SIMD_HAS_INT4
+template<>
+struct hash<::simd::int4>
+{
+	size_t operator()(const ::simd::int4& v) const noexcept
+	{
+        return ::simd::hash(v);
+    }
+};
+#endif /* SIMD_HAS_INT4 */
 
 } // namespace std
-
-#endif /* SIMD_HAS_FLOAT4 */
